4822cce10b3edf54d3e8a593c456c98a17269416
Sekkai-Wassim <wassim@sekkai-consulting.com>
2026-07-01 13:51:39 +0000

    added the config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+
+  WEBAPIURL: 'https://sekkai-consulting-pivision.com/PIWEBAPI',
+
+  AFServer:"EC2AMAZ-4F8UROL",
+
+  DB_Name : "Yara Laboratory",
+
+  AF_DATABASE_PATH:'\\\\EC2AMAZ-4F8UROL\\Yara Laboratory',
+
+  Level1_Template:"DataEntryTemplate"
+
+//   REFRESH_SEC: 10,
+
+//   CREDENTIALS_MODE: 'include',
+
+  
+
+};
